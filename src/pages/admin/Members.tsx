ffa3de1caea882e141227f352c477ced30ad7625
Dongkyun Ham <jokerham@gmail.com>
2024-08-08 07:52:34 +0900

    added aws sdk config.

--- src/pages/admin/Members.tsx
+++ src/pages/admin/Members.tsx
@@ -1,27 +1,22 @@
-import { Button, ButtonGroup, Box, Divider, Toolbar, Table, TableContainer, TableHead, TableRow, TableCell } from '@mui/material';
-import { ButtonText } from 'component/CustomMaterialUI';
-import ButtonGroupText from 'component/CustomMaterialUI/ButtonGroupText';
+import { Button, ButtonGroup, Box, Divider, Toolbar, Table, TableContainer, TableHead, TableRow, TableCell, ToggleButtonGroup, ToggleButton, TableBody, Paper } from '@mui/material';
 import { useState, useEffect } from 'react';
 import AWS from 'aws-sdk';
 import 'amplifyconfigure';
-import config from 'aws-exports.js';
+import config from 'aws-exports';
+import awsconfig from 'aws-config';
 
 const Members = () => {
-  const [members, setMembers] = useState([]);
+  const [members, setMembers] = useState<AWS.CognitoIdentityServiceProvider.UsersListType | undefined>();
+  const [filter, setFilter] = useState<string | null>('all');
+
   useEffect(() => {
     const fetchMembers = async () => {
       try {
-        AWS.config.update({
-          region: config.aws_project_region, // Replace with your actual AWS region
-          credentials: new AWS.Credentials({
-            accessKeyId: '',
-            secretAccessKey: 'your-secret-access-key'
-          })
-        });
+        AWS.config.update(awsconfig);
         const cognitoISP = new AWS.CognitoIdentityServiceProvider();
         const params = {
           //region: config.aws_project_region,
-          UserPoolId: config.aws_cognito_identity_pool_id, // Replace with your user pool ID
+          UserPoolId: config.aws_user_pools_id, // Replace with your user pool ID
           // Optional: Filter and pagination options
           // Limit: 10, // Specify number of users to return
           // PaginationToken: 'token' // For fetching subsequent results
@@ -34,7 +29,7 @@ const Members = () => {
         };
         const response = await cognitoISP.listUsers(params).promise();
         console.log('Cognito Users:', response.Users);
-        return response.Users;
+        setMembers(response.Users);
       } catch (error) {
         console.error('Error listing users:', error);
         throw error;
@@ -44,30 +39,45 @@ const Members = () => {
     fetchMembers();
   }, []);
 
+  const onChangeFilterHandler = (_event: React.MouseEvent<HTMLElement>, value: string | null) => {
+    setFilter(value);
+  };
+
   return (
     <Box>
       <h1>Members</h1>
       <Divider />
       <Toolbar variant="dense" sx={{ displya: 'flex', justifyContent: 'space-between', px: { sm: 0 } }}>
-        <ButtonGroupText>
-          <ButtonText>All Members</ButtonText>
-          <ButtonText>Admin</ButtonText>
-          <ButtonText>Approved</ButtonText>
-          <ButtonText>Rejected</ButtonText>
-        </ButtonGroupText>
+        <ToggleButtonGroup exclusive size="small" value={filter} onChange={onChangeFilterHandler}>
+          <ToggleButton value="all">All Members</ToggleButton>
+          <ToggleButton value="admin">Admin</ToggleButton>
+          <ToggleButton value="approved">Approved</ToggleButton>
+          <ToggleButton value="rejected">Rejected</ToggleButton>
+        </ToggleButtonGroup>
         <ButtonGroup>
           <Button size="medium" variant="contained"><Box>Add</Box></Button>
           <Button size="medium" variant="contained"><Box>Modify</Box></Button>
           <Button size="medium" variant="contained"><Box>Delete</Box></Button>
         </ButtonGroup>
       </Toolbar>
-      <TableContainer>
+      <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>1</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Nickname</TableCell>
+              <TableCell>Email</TableCell>
             </TableRow>
           </TableHead>
+          <TableBody>
+            {members?.map((member, index) => (
+              <TableRow key={index}>
+                <TableCell>{member.Attributes?.find(attr => attr.Name === 'name')?.Value}</TableCell>
+                <TableCell>{member.Attributes?.find(attr => attr.Name === 'nickname')?.Value}</TableCell>
+                <TableCell>{member.Attributes?.find(attr => attr.Name === 'email')?.Value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
         </Table>
       </TableContainer>
     </Box >
